937e1648ec8d0b4049b5748e714c387a722faa66
Marcos-PCastro <courrielprive@yahoo.com.br>
2023-03-10 12:17:08 -0300

    Working with four verbs

---
+++ RestWithASP-NET5/Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASP_NET5.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Address { get; set; }
+        public String Gender { get; set; }
+
+
+    }
+}
